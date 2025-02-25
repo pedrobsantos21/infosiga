044c11401b6c251b1a4d9dd2c93d6efc3dcc52fd
pabsantos <pabsantos21@gmail.com>
2025-02-25 17:21:08 -0300

    Cria o dataset de veiculos (#3)

--- data-raw/infosiga_veiculos.R
+++ data-raw/infosiga_veiculos.R
@@ -1,5 +1,14 @@
-## code to prepare `infosiga_veiculos` dataset goes here
+tempdir = tempdir()
+path_zip = "data-raw/dados_infosiga.zip"
+unzip(path_zip, exdir = tempdir)
+path_veiculos = list.files(tempdir, pattern = "^veiculos", full.names = TRUE)
 
+veiculos = readr::read_csv2(
+    path_veiculos,
+    locale = readr::locale(encoding = "latin1")
+)
+
+unlink(tempdir, recursive = TRUE)
 
 
 usethis::use_data(infosiga_veiculos, overwrite = TRUE)
